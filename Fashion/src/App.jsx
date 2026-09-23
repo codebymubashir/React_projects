f931bac6b16a996421905c9e937537591949c3fb
codebymubashir <unimubashir2@gmail.com>
2026-09-23 08:52:52 -0700

    installing react and tailwind css for botique

---
+++ Fashion/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white font-bold text-2xl'>
+      welcome to my fashion website
+    </div>
+  )
+}
+
+export default App
